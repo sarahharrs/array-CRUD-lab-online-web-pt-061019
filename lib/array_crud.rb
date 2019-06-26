cc55063c246df1524f37bfb2c8b8611166a348fe
Sarah Harrs <sarahharrs@gmail.com>
2019-06-26 16:17:43 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,39 @@
 def create_an_empty_array
-  
+  [ ] 
 end
 
 def create_an_array
-  
+  new_array = ["beans", "roast", "dark","light"] 
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(new_array_1, "sugar")
+  new_array_1 = [ "creamer", "splenda", "stevia", "monkfruit"]
+  new_array_1.push.("sugar")
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(new_array_2, "blush")
+  new_array_2 = ["lipstick", "iron", "lashes","mascara"]
+  new_array_2.unshift.("blush")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(new_array_1)
+  new_array_1.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(new_array_2)
+  new_array_2.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(number_array, 2)
+  number_array = [ "Mallory", "Baloo", "Herman", "Monica")
+  number_array[2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(number_array)
+  number_array.index.[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(index_array)
+  index_array = ""paper", "glue", "pencils"]
+  index_array.index[-1]
 end
